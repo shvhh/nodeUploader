5cac2564317dcbfb3a3df676ce06552caf9f4686
Hemant Rajpoot <hemant.rajpoot@unthinkable.co>
2020-02-27 14:13:20 +0530

    send env varible

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var path = require('path');
 var directory = 'uploads';
 
-console.log(JSON.stringify(process.env));
+console.log(JSON.stringify());
 var storage = multer.diskStorage({
   destination: function(req, file, cb) {
     cb(null, directory);
@@ -24,7 +24,7 @@ var upload = multer({ storage });
 var app = express();
 app.use(express.static(directory));
 app.post('/upload', upload.single('file'), function(req, res, next) {
-  res.send(req.file);
+  res.send(process.env);
 });
 
 app.listen(process.env.PORT || 8000);
